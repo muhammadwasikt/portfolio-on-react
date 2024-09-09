c6bda77d8ba773b2d6d8a1ec4a9fd994e133b304
Muhammad Wasi <wasikhatri11@gmail.com>
2024-09-09 14:12:47 -0700

    resume-ubdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import Header from "./component/Header/Header"
-import Home from "./component/pages/Home/Home";
 
-const App = ()=>{
-  return(
-    <>
-    <Header/>
-    <Home />
-    </>
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Service from './components/Service'
+
+function App() {
+
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Service />
+        <Projects />
+        <Contact />
+        <Footer />
+    </div>
   )
 }
-export default App;
+
+export default App
