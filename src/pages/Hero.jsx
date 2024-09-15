8a78841737a1b064c43b4a121c1ed8319845f87f
Muhammad Wasi <wasikhatri11@gmail.com>
2024-09-15 22:46:59 +0500

    sending-mail

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,6 +5,9 @@ const Hero = () => {
   const clicks = ()=>{
     window.open('https://firebasestorage.googleapis.com/v0/b/portfolio-3147e.appspot.com/o/My%20Resume.pdf?alt=media&token=bd553e49-8ee0-448c-b375-7b68ba5b0082', '_blank')
   }
+  const sendMail = ()=>{
+    window.location.href = "mailto:wasikhatri11@gmail.com?subject=Inquiry from Portfolio&body=Hello Wasi,";
+  }
   return (
     <div className='bg-black text-white text-center py-16'>
         <img src={HeroImage} alt="" 
@@ -19,7 +22,7 @@ const Hero = () => {
             I specialize in building modern and responsive web applications.
         </p>
         <div className='mt-8 space-x-4'>
-            <Button title='Contact with me' />
+            <Button title='Contact with me' click={sendMail} />
             <Button title='Resume' isClassName click={clicks} />
         </div>
 
