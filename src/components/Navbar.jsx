2a3eb95a80ee1bd3441564db5c9c82597096fd41
Muhammad Wasi <wasikhatri11@gmail.com>
2024-09-19 22:21:40 +0500

    icons-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,7 @@ const Navbar = () => {
 const [isResponsive , setIsResponsive] = useState(false)
 
 const click = ()=>{
-  setIsResponsive(true)
-}
-const click2 = ()=>{
-  setIsResponsive(false)
+  setIsResponsive(!isResponsive)
 }
 const sendMail = ()=>{
   window.location.href = "mailto:wasikhatri11@gmail.com?subject=Inquiry from Portfolio&body=Hello Wasi,";
@@ -21,19 +18,18 @@ let resposive = ` w-[600%] h-[300px] flex flex-col leading-[45px] mx-auto my-10 
 
   return (
     <nav className='sticky top-0 bg-[#080808] z-10 text-white px-8 md:px-16 lg:px-24 flex justify-center '>
-        <div className={isResponsive ? 'bg-black absolute z-10 container py-2 flex justify-center md:justify-between ':'container py-2 flex justify-center md:justify-between items-center'}>
+        <div className={isResponsive ? 'bg-black absolute container py-2 px-8 flex justify-center md:justify-between ':'container py-2 flex justify-center md:justify-between items-center'}>
             <div className='text-2xl font-bold '>WASI</div>
             <div className={isResponsive ? resposive : `space-x-6 xl:inline-flex md:inline-flex sm: hidden items-center`}>
-                <NavLink onClick={click2} to='/home' className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Home</NavLink>
-                <NavLink onClick={click2} to="/about" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>About Me</NavLink>
-                <NavLink onClick={click2} to="/services" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Services</NavLink>
-                <NavLink onClick={click2} to="/project" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Projects</NavLink>
-                <NavLink onClick={click2} to="/contact" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Contact</NavLink>
+                <NavLink onClick={click} to='/home' className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Home</NavLink>
+                <NavLink onClick={click} to="/about" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>About Me</NavLink>
+                <NavLink onClick={click} to="/services" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Services</NavLink>
+                <NavLink onClick={click} to="/project" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Projects</NavLink>
+                <NavLink onClick={click} to="/contact" className={isResponsive ? 'hover:text-gray-400 border-b-2':'hover:text-gray-400'}>Contact</NavLink>
                 <Button title='Contact Me' isResponsive={isResponsive} click={sendMail} />
             </div>
             <div className="xl:hidden md:hidden text-3xl flex justify-end w-11/12">
-                <HiMiniBars3BottomRight className={ isResponsive && "hidden"} onClick={click} />
-                <AiOutlineCloseCircle className={isResponsive ? 'inline-flex': 'hidden'} onClick={click2} />
+                {isResponsive ? <AiOutlineCloseCircle onClick={click}/> : <HiMiniBars3BottomRight onClick={click}/>}
             </div>
         </div>
     </nav>
